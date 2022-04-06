cdfdb33f7294d247636207f1ade080131e3263ff
zhi_hui <wang-zhi_hui@qq.com>
2022-04-06 16:43:03 +0800

    .

---
+++ javascript/Lang/cloneDeep.js
@@ -0,0 +1,30 @@
+export const cloneDeep = (value) => {
+  const type = typeof value;
+  if (!["object", "function"].includes(type) || value === null) return value;
+
+  const _type = Object.prototype.toString.call(value);
+  let result;
+  switch (_type) {
+    case "[object Array]":
+      result = []
+      break;
+    case "[object Function]":
+      result = eval(`(${value.toString()})`)
+      break;
+    case "[object RegExp]":
+      result = new RegExp(value);
+      break;
+    case "[object Date]":
+      result = new Date(value);
+      break;
+    case "[object Object]":
+      result = new Object();
+      break;
+  }
+  for (let key in value) {
+    if (value.hasOwnProperty(key)) {
+      result[key] = cloneDeep(value[key])
+    }
+  }
+  return result;
+};

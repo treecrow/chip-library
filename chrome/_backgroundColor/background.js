45d00f04d2d887bcd4adf10f14addad4511fdca7
zhi_hui <wang-zhi_hui@qq.com>
2022-04-25 18:43:39 +0800

    。

--- chrome/_backgroundColor/background.js
+++ chrome/_backgroundColor/background.js
@@ -1,9 +1,14 @@
-const { runtime } = chrome;
+const { runtime, action, storage, tabs } = chrome;
 
-chrome.runtime.onInstalled.addListener((details) => {
+runtime.onInstalled.addListener((details) => {
   // 自动打开chrome扩展介绍文件
-  chrome.tabs.create({ url: runtime.getURL("README.md") });
+  tabs.create({ url: runtime.getURL("README.md") });
 
   // 初始数据设置
-  chrome.storage.sync.set({ color: "red" });
+  storage.sync.set({ color: "red" });
+});
+
+// 点击扩展icon时
+action.onClicked.addListener((tab) => {
+  // do something
 });
